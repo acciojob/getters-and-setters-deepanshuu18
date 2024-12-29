6a4a7a048803f484bf230147e8105c536766bdf6
DEEPANSHU <deepanshufea07@gmail.com>
2024-12-29 21:52:21 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,33 +1,47 @@
 //complete this code
 class Person {
-	constructor(  name,age){
-		this._name=name;
-		this._age=age;
-	}
-	get name(){
-		return this._name;
-	}
-	set age(){
-		return this._age;
-	}
-	
+  constructor(name, age) {
+    this._name = name;
+    this._age = age;
+  }
+
+  // Getter for name
+  get name() {
+    return this._name;
+  }
+
+  // Getter for age
+  get age() {
+    return this._age;
+  }
+
+  // Setter for age (sets the internal _age variable)
+  set age(age) {
+    this._age = age;
+  }
 }
 
 class Student extends Person {
-	constructor(name,age){
-		super(name,age)
-	}
-	study(){
-	 console.log(`${this.name} is studying`);
-	}
+  constructor(name, age) {
+    super(name, age);
+  }
+
+  // Method for studying
+  study() {
+    console.log(`${this.name} is studying`);
+  }
 }
 
-class Teacher extends Person 
- {
-	constructor(name,age){
-		super(name,age)
-	}
-	 teach(){console.log(`${this.name}is teaching`)}
+class Teacher extends Person {
+  constructor(name, age) {
+    super(name, age);
+  }
+
+  // Method for teaching
+  teach() {
+    console.log(`${this.name} is teaching`);
+  }
+}
 
 // Do not change the code below this line
 window.Person = Person;
